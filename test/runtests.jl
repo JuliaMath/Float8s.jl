e1eeb70568746baf2b97ac2ae6f94b236f65fb9c
milankl <milankloewer@gmx.de>
2020-02-11 17:51:08 +0000

    F32->F8 also working for subnormals, only Float8

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Test
 
 @testset "Conversion to Float32" begin
 
-    for i in 0x10:0x80
-        @test i == reinterpret(UInt8,Float8(Float32(Float8(i))))
+    for i in 0x00:0xff
+        if ~isnan(Float8(i))
+            @test i == reinterpret(UInt8,Float8(Float32(Float8(i))))
+        end
     end
 end
